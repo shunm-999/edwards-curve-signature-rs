1b36dc500038a46d75b79d9451d0f411eed5c0cb
shunm-999 <shunmiyazaki7812@gmail.com>
2025-10-20 23:22:58 +0900

    Add Ed25519 struct with SHA-512 implementation and update dependencies

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -1,3 +1,12 @@
-pub(crate) struct Ed25519 {
+use sha2::{Digest, Sha512};
 
+pub(crate) struct Ed25519 {}
+
+impl Ed25519 {
+    fn sha512(s: &[u8]) -> [u8; 64] {
+        let mut sha512 = Sha512::default();
+
+        sha512.update(s);
+        sha512.finalize_reset().into()
+    }
 }
