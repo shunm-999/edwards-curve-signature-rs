dcee8ffb3dc62168cd567e8727132d2fa26e9e26
shunm-999 <shunmiyazaki7812@gmail.com>
2025-11-29 21:52:59 +0900

    Refactor signing logic to improve readability and maintainability

--- src/main.rs
+++ src/main.rs
@@ -19,32 +19,41 @@ fn main() -> io::Result<()> {
             secret_file_path,
             output_file_path,
         } => {
-            let signer = EdDsaSignature::new(SignatureAlgorithm::Ed25519);
-
-            let message_reader = match message_file_path {
-                Some(ref path) => MessageReader::File(path.clone()),
-                None => MessageReader::Stdin,
-            };
-            let secret_reader = if secret_file_path.ends_with(".pem") {
-                SecretReader::PemFile(secret_file_path)
-            } else {
-                unimplemented!("Only PEM secret files are supported");
-            };
-            let signature_writer = match output_file_path {
-                Some(ref path) => SignatureWriter::File(path.clone()),
-                None => SignatureWriter::Stdout,
-            };
-
-            let message = message_reader.read_message()?;
-            let secret = secret_reader.read_secret()?;
-
-            let signature = signer.sign(&secret, &message).ok_or_else(|| {
-                io::Error::new(io::ErrorKind::Other, "Failed to sign the message")
-            })?;
-
-            signature_writer.write_signature(&signature)?;
+            sign(message_file_path, secret_file_path, output_file_path)?;
         }
     }
 
+    Ok(())
+}
+
+fn sign(
+    message_file_path: Option<String>,
+    secret_file_path: String,
+    output_file_path: Option<String>,
+) -> io::Result<()> {
+    let signer = EdDsaSignature::new(SignatureAlgorithm::Ed25519);
+
+    let message_reader = match message_file_path {
+        Some(ref path) => MessageReader::File(path.clone()),
+        None => MessageReader::Stdin,
+    };
+    let secret_reader = if secret_file_path.ends_with(".pem") {
+        SecretReader::PemFile(secret_file_path)
+    } else {
+        unimplemented!("Only PEM secret files are supported");
+    };
+    let signature_writer = match output_file_path {
+        Some(ref path) => SignatureWriter::File(path.clone()),
+        None => SignatureWriter::Stdout,
+    };
+
+    let message = message_reader.read_message()?;
+    let secret = secret_reader.read_secret()?;
+
+    let signature = signer
+        .sign(&secret, &message)
+        .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Failed to sign the message"))?;
+
+    signature_writer.write_signature(&signature)?;
     Ok(())
 }
