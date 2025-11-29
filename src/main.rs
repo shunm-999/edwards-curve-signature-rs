2ab4b6e35f97baedf48d56775c9c39070332d1bc
shunm-999 <shunmiyazaki7812@gmail.com>
2025-11-29 21:34:47 +0900

    Add support for reading PEM secret files in CLI signing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::cli::{Args, ReadMessage};
+use crate::cli::{Args, ReadMessage, ReadSecret};
 use clap::Parser;
 use std::io;
 mod algorithm;
@@ -17,8 +17,14 @@ fn main() -> io::Result<()> {
                 Some(ref path) => cli::MessageReader::File(path.clone()),
                 None => cli::MessageReader::Stdin,
             };
+            let secret_reader = if secret_file_path.ends_with(".pem") {
+                cli::SecretReader::PemFile(secret_file_path)
+            } else {
+                unimplemented!("Only PEM secret files are supported");
+            };
 
             let message = message_reader.read_message()?;
+            let secret = secret_reader.read_secret()?;
 
             let message = String::from_utf8(message).unwrap();
             println!("Signing message: {}", message);
